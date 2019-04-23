d3521bb96a57e9cbccb2fcda06466a91064e2953
x3v <jonzevallos@gmail.com>
2019-04-23 11:15:32 -0500

    Add files via upload
    If an email list is provided this will search AD for the email address then add the requested users to a group. Else when the user is not found they populate within a text file with all failed users.

---
+++ AD Automation/add-admemberemail.ps1
@@ -0,0 +1,15 @@
+import-module activedirectory 
+
+$csv = Import-CSV .\group.csv
+foreach($user in $csv){  
+    $userMail = $user.group
+    $sam = Get-ADUser -Filter {mail -eq $userMail} | Select SamAccountName | ft -HideTableHeaders | Out-String
+    if ($sam){ 
+    $sam = $sam.name.replace("@{SamAccountName=","")
+    $sam = $sam.name.replace("}","")
+    $sam= $sam.Trim()
+    % {add-adgroupmember -identity "INT sZscaler_PPIB" -members $sam}
+    }else{
+    Write-Output $user.group | Out-File -FilePath .\fail.txt -Append
+    }
+}
